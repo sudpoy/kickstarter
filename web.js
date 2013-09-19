994fc82c8ea5955cf6a7a6eb55ed84b01f10cb59
Jin Songbo <sudpojin@Songbos-iMac.local>
2013-09-19 09:20:23 -0400

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+
+var app = express.createServer(express.logger());
+
+var fs = require('fs');                                                                                               
+var infile = "index.html";
+
+var file_contents = fs.readFileSync(infile,"utf-8");
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(file_contents);
 });
 
 var port = process.env.PORT || 5000;
